e6da2044660db787439ccd2288f9767619efbee4
Ziad Ahmed Mustafa <141859084+ziadMuustafa@users.noreply.github.com>
2024-12-26 16:55:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using EF_LibManagmentProject.MyContext;
+
+namespace EF_LibManagmentProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var contxt = new AppDbContext();
+
+        }
+    }
+}
